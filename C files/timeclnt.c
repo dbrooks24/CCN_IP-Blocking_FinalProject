df873034f0c2c168f21958b19b78f65920289ac9
David Brooks <davidabrooks24@aol.com>
2021-11-17 23:54:09 -0500

    Update timeclnt.c

--- C files/timeclnt.c
+++ C files/timeclnt.c
@@ -28,48 +28,51 @@ int main(int ac, char *av[])
       */
 	printf("Step 1 started...\n"); //DBROOKS - added
 	
-	printf("IP passed: %s\n", av[1]); //DBROOKS - added
+		printf("IP that was passed: %s\n", av[1]); //DBROOKS - added
 
-	sock_id = socket( AF_INET, SOCK_STREAM, 0 );    /* get a line   */
-	if ( sock_id == -1 ) 
-		oops( "socket" );          		/* or fail      */
+		sock_id = socket( AF_INET, SOCK_STREAM, 0 );    /* get a line   */
+		if ( sock_id == -1 ) 
+			oops( "socket" );          		/* or fail      */
+	
 	printf("Step 1 ended...\n\n"); //DBROOKS - added
 
+	
+	
+	/*
+	* Step 2: connect to server
+	*         need to build address (host,port) of server  first
+	*/
 	printf("Step 2 started...\n"); //DBROOKS - added
-     /*
-      * Step 2: connect to server
-      *         need to build address (host,port) of server  first
-      */
+		
+		bzero( &servadd, sizeof( servadd ) );   /* zero the address     */
 
-	bzero( &servadd, sizeof( servadd ) );   /* zero the address     */
+		hp = gethostbyname( av[1] );            /* lookup host's ip #   */
+		if (hp == NULL) 
+			oops(av[1]);            	/* or die               */
+		bcopy(hp->h_addr, (struct sockaddr *)&servadd.sin_addr, hp->h_length);
 
-	hp = gethostbyname( av[1] );            /* lookup host's ip #   */
-	if (hp == NULL) 
-		oops(av[1]);            	/* or die               */
-	bcopy(hp->h_addr, (struct sockaddr *)&servadd.sin_addr, hp->h_length);
+		servadd.sin_port = htons(atoi(av[1]));  /* fill in port number  */ // DBROOKS - changed 'av[2]' to av[1], as seg fault was present
 
-	servadd.sin_port = htons(atoi(av[1]));  /* fill in port number  */ // DBROOKS - changed 'av[2]' to av[1], as seg fault was present
+		servadd.sin_family = AF_INET ;          /* fill in socket type  */
 
-	servadd.sin_family = AF_INET ;          /* fill in socket type  */
-
-						       /* now dial     */
-	if ( connect(sock_id,(struct sockaddr *)&servadd, sizeof(servadd)) !=0)
+		/* now dial     */
+		if ( connect(sock_id,(struct sockaddr *)&servadd, sizeof(servadd)) !=0)
 	       oops( "connect" );
-		   
+
 	printf("Step 2 ended...\n\n"); //DBROOKS - added
 
 
-     /*
-      * Step 3: transfer data from server, then hangup
-      */
+	/*
+	* Step 3: transfer data from server, then hangup
+	*/
 	printf("Step 3 started...\n"); //DBROOKS - added
 
-	messlen = read(sock_id, message, BUFSIZ);     /* read stuff   */
-	if ( messlen == - 1 )
-	       oops("read") ;
-	if ( write( 1, message, messlen ) != messlen )  /* and write to */
-	       oops( "write" );                        /* stdout       */
-	close( sock_id );
+		messlen = read(sock_id, message, BUFSIZ);     /* read stuff   */
+		if ( messlen == - 1 )
+	       	oops("read") ;
+		if ( write( 1, message, messlen ) != messlen )  /* and write to */
+	       	oops( "write" );                        /* stdout       */
+		close( sock_id );
 
 	printf("Step 3 ended, closed socket...\n"); //DBROOKS - added
 }
